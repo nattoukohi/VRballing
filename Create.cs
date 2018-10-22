bc122b7ac003422040afdf5eae4f74a3ada030cd
Owarokohi <juno_diamondpearl@yahoo.co.jp>
2018-10-22 18:17:21 +0900

    Initial commit

---
+++ Create.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Create : MonoBehaviour {
+    private SteamVR_TrackedObject trackedObj;
+
+    public GameObject Object;
+
+    public Transform Objecttrans;
+
+    private SteamVR_Controller.Device Controller
+    {
+        get { return SteamVR_Controller.Input((int)trackedObj.index); }
+    }
+
+    void Awake()
+    {
+        //Debug.Log("eee");
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Controller.GetPressDown(SteamVR_Controller.ButtonMask.Grip)){
+            GameObject obj = Instantiate(Object, Objecttrans.transform.position, Objecttrans.rotation) as GameObject;
+        }	
+	}
+}
---
+++ LaserPointer.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace balling
+{
+
+        public class LaserPointer : MonoBehaviour
+        {
+        //
+        [SerializeField] SceneManagement SceneTransfer;
+        private SteamVR_TrackedObject trackedObj;
+            // 1
+            public GameObject laserPrefab;
+            // 2
+            private GameObject laser;
+            // 3
+            private Transform laserTransform;
+            // 4
+            private Vector3 hitPoint;
+
+            private SteamVR_Controller.Device Controller
+            {
+                get { return SteamVR_Controller.Input((int)trackedObj.index); }
+            }
+
+            private void Start()
+            {
+                // 1
+                laser = Instantiate(laserPrefab);
+                // 2
+                laserTransform = laser.transform;
+            }
+
+            private void Update()
+            {
+
+                RaycastHit hit;
+
+                // 2
+                if (Physics.Raycast(trackedObj.transform.position, transform.forward, out hit, 100))
+                {
+                    hitPoint = hit.point;
+                    ShowLaser(hit);
+                    //Destroy(hit.collider.gameObject);
+                    if (Controller.GetPress(SteamVR_Controller.ButtonMask.Trigger))
+                    {
+                        if(hit.collider.gameObject.tag == "GameStart")
+                        SceneTransfer.StartGame();
+                    }
+                }
+                else
+                {
+                    laser.SetActive(false);
+                }
+
+
+            }
+
+            private void ShowLaser(RaycastHit hit)
+            {
+                // 1
+                laser.SetActive(true);
+                // 2
+                laserTransform.position = Vector3.Lerp(trackedObj.transform.position, hitPoint, .5f);
+                // 3
+                laserTransform.LookAt(hitPoint);
+                // 4
+                laserTransform.localScale = new Vector3(laserTransform.localScale.x, laserTransform.localScale.y,
+                    hit.distance);
+            }
+
+            void Awake()
+            {
+                trackedObj = GetComponent<SteamVR_TrackedObject>();
+            }
+        }
+
+    
+}
+
