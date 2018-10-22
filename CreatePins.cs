9b1f317fc48479aba980d7dcc477fde7e9ab573b
Owarokohi <juno_diamondpearl@yahoo.co.jp>
2018-10-22 18:54:22 +0900

    pincreate

---
+++ CreatePins.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatePins : MonoBehaviour {
+    public GameObject [] Object = new GameObject [10];
+
+    // Use this for initialization
+    void Start () {
+        CreateThePins();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void CreateThePins()
+    {
+        foreach(GameObject x in Object)
+        {
+            Instantiate(x);
+        }
+        
+    }
+}
